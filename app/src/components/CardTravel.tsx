c5e376b6a0e307872a7f7848cb61f87434700512
MDorizon <mdorizon@proton.me>
2024-10-21 11:58:56 +0200

    :sparkles: travelListing

--- app/src/components/CardTravel.tsx
+++ app/src/components/CardTravel.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { TravelType } from "../types/travel.type";
 
 type CardTravelProps = {
@@ -8,10 +9,14 @@ const CardTravel = ({ travel }: CardTravelProps) => {
     return (
         <div className="shadow-md">
             <div className="h-[200px] overflow-hidden">
-                <img src={travel.image} alt={`picture of ${travel.name}`} />
+                <Link to={`/${travel.id}`}>
+                    <img src={travel.image} alt={`picture of ${travel.name}`} />
+                </Link>
             </div>
-            <div className="p-2">
-                <h2 className="text-xl text-zinc-500 fond-bold mt-2">{travel.name}</h2>
+            <div className="p-6">
+                <Link to={`/${travel.id}`}>
+                    <h2 className="text-xl text-zinc-500 fond-bold mt-2">{travel.name}</h2>
+                </Link>
                 <p>
                     {travel.description.substring(0, 50)}
                     {travel.description.length > 50 && "..."}
--- app/src/pages/TravelListPage.tsx
+++ app/src/pages/TravelListPage.tsx
@@ -2,7 +2,7 @@ import TravelList from "../components/TravelList";
 
 const TravelListPage = () => {
     return (
-        <div className="container mx-auto">
+        <div>
             <h1 className="text-4xl text-red-400 mb-10">Share your travel</h1>
             <TravelList />
         </div>
